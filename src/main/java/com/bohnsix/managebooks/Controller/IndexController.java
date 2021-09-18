6fdfcd04a688a1c8c2916c28e70ed3719a4f8a06
bohnsix <bohn_six@163.com>
2021-09-18 12:18:18 +0800

    add some pojo

---
+++ src/main/java/com/bohnsix/managebooks/Controller/IndexController.java
@@ -0,0 +1,44 @@
+package com.bohnsix.managebooks.Controller;
+
+
+import com.bohnsix.managebooks.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value={"/", "/login"})
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String mainPage(User user, HttpSession session, Model model) {
+        if(!"".equals(user.getUserName()) && "123456".equals(user.getPassword())) {
+            session.setAttribute("loginUser", user);
+            return "redirect:/main.html";
+        }
+        else {
+            model.addAttribute("msg", "账号密码错误");
+            return "login";
+        }
+    }
+
+    @GetMapping("/main.html")
+    public String mainPage(HttpSession session, Model model) {
+        Object user = session.getAttribute("loginUser");
+        if(user != null) {
+            return "main";
+        }
+        else {
+            model.addAttribute("msg", "请重新登陆");
+            return "login";
+        }
+    }
+}
---
+++ src/test/java/com/bohnsix/managebooks/ManageBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohnsix.managebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
